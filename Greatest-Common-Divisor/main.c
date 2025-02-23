47b4ebca629c6be5d054f59aecbe0b0481404925
Arseniy Malyuk <arsenmaliuk12@gmail.com>
2025-02-23 22:36:39 +0100

    Added Greatest Common Divisor task

---
+++ Greatest-Common-Divisor/main.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct {
+    long unsigned int a;
+    long unsigned int b;
+    long unsigned int res;
+} Numbers;
+
+int main(){
+    long unsigned int N;
+    Numbers *arr;
+
+    scanf ("%lu", &N);
+
+    arr = (Numbers*)malloc(N * sizeof(Numbers));
+
+    for (int i = 0; i < N; i++) {
+        scanf("%lu %lu", &arr[i].a, &arr[i].b);
+    }
+
+    for (int i = 0; i < N; i++) {
+        while (arr[i].a && arr[i].b) {
+            if (arr[i].a > arr[i].b) {
+                arr[i].a = arr[i].a % arr[i].b;
+            } else {
+                arr[i].b = arr[i].b % arr[i].a;
+            }
+        }
+        
+        if (arr[i].a) {
+            arr[i].res = arr[i].a;
+        } else{
+            arr[i].res = arr[i].b;
+        }
+    }
+        
+    for (int i = 0; i < N; i++) {
+        if (i == N - 1) {
+            printf ("%lu", arr[i].res);
+        } else {
+            printf ("%lu\n", arr[i].res);
+        }
+    }
+    free(arr);
+
+    return 0;
+}
